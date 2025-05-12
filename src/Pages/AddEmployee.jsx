b86f61e1271acef67059ca703a91010a45ab42c3
sougata <sougata19582001@gmail.com>
2025-05-12 22:18:12 +0530

    update the default role

--- src/Pages/AddEmployee.jsx
+++ src/Pages/AddEmployee.jsx
@@ -1,3 +1,4 @@
+import { CgArrowsExchange } from "react-icons/cg";
 import { Button } from "@/components/ui/button";
 import {
   Drawer,
@@ -36,6 +37,16 @@ import {
 } from "@/components/ui/dropdown-menu";
 import Select from "react-select";
 import makeAnimated from "react-select/animated";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 const animatedComponents = makeAnimated();
 
@@ -255,6 +266,7 @@ const EmployeeActionsMenu = ({ id }) => {
       <DropdownMenuContent className="w-56">
         <DropdownMenuGroup>
           <AddRoleDrawer employeeId={id} />
+          <DefaultRoleChange employeeId={id} />
           <DropdownMenuItem>
             <div
               className={
@@ -358,7 +370,7 @@ const AddRoleDrawer = ({ employeeId }) => {
       const payloadArray = rolesToAssign.map((rta) => ({
         employee: { id: employeeId },
         role: { id: rta.value.id },
-        isDefault: selectedDefaultRole.value.id === rta.value.id ? 1 : 0,
+        isDefault: selectedDefaultRole?.value.id === rta.value.id ? 1 : 0,
       }));
       const response = await axios.post(
         import.meta.env.VITE_SERVER_URL + "/employee-role-map/bulk",
@@ -479,6 +491,137 @@ const AddRoleDrawer = ({ employeeId }) => {
                   />
                 </div>
               )}
+            </div>
+            <DrawerFooter>
+              <Button>Submit</Button>
+              <DrawerClose asChild>
+                <Button variant="outline">Cancel</Button>
+              </DrawerClose>
+            </DrawerFooter>
+          </form>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+const DefaultRoleChange = ({ employeeId }) => {
+  const defaultRoleRef = useRef(null);
+  const [defaultRole, setDefaultRole] = useState(null);
+  const [assignedRoles, setAssignedRoles] = useState([]);
+  const [newDefaultRole, setNewDefaultRole] = useState(null);
+
+  const fetchDefaultRole = useCallback(async () => {
+    const response = await axios.get(
+      import.meta.env.VITE_SERVER_URL +
+        `/employee-role-map/default-role/${employeeId}`
+    );
+    if (response.status === 200) {
+      setDefaultRole({
+        label: response.data.roleName,
+        value: { ...response.data },
+      });
+    }
+  }, [employeeId]);
+
+  const fetchAssignedRoles = useCallback(async () => {
+    try {
+      const response = await axios.get(
+        import.meta.env.VITE_SERVER_URL +
+          `/employee-role-map?employeeId=${employeeId}`
+      );
+      const data = response.data.map((d) => ({
+        label: d.role.roleName,
+        value: d.role,
+      }));
+      setAssignedRoles(data);
+    } catch (error) {
+      toast.error("Error", { description: error.message });
+    }
+  }, [employeeId]);
+
+  useEffect(() => {
+    (async () => {
+      await fetchDefaultRole();
+      await fetchAssignedRoles();
+    })();
+  }, [fetchDefaultRole, fetchAssignedRoles]);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const payload = {
+        employeeId,
+        newRoleId: newDefaultRole.value.id,
+        oldRoleId: defaultRole.value.id,
+      };
+      console.log(payload);
+
+      const response = await axios.put(
+        import.meta.env.VITE_SERVER_URL +
+          "/employee-role-map/update-default-role",
+        payload
+      );
+      const _ = response.data;
+      setNewDefaultRole(null);
+      await fetchDefaultRole();
+      await fetchAssignedRoles();
+      defaultRoleRef.current.clearValue();
+      toast.success("Success", { description: "Default Role Updated!" });
+    } catch (error) {
+      toast.error("Error", { description: error.message });
+    }
+  }
+
+  return (
+    <Drawer>
+      <DrawerTrigger asChild>
+        <DropdownMenuItem
+          onSelect={(e) => e.preventDefault()} // prevents menu from closing
+          className="cursor-pointer flex items-center gap-2"
+        >
+          <CgArrowsExchange />
+          <div>Default Roles</div>
+        </DropdownMenuItem>
+      </DrawerTrigger>
+      <DrawerContent className="min-h-[60vh] max-h-[100vh]">
+        <div className="mx-auto w-full max-w-md">
+          <DrawerHeader>
+            <DrawerTitle>Default Role</DrawerTitle>
+            <DrawerDescription>
+              Give access based on default role
+            </DrawerDescription>
+          </DrawerHeader>
+          <form onSubmit={handleSubmit}>
+            <div className="flex flex-col items-center justify-center gap-3.5 my-5">
+              {defaultRole && (
+                <div className={"w-full flex flex-col gap-2.5"}>
+                  <Label>Default Role</Label>
+                  <div className={"flex gap-2 flex-wrap"}>
+                    <div
+                      className={
+                        "shadow bg-gray-100 flex gap-2.5 justify-between items-center px-2 rounded-2xl"
+                      }
+                    >
+                      {defaultRole.label}
+                    </div>
+                  </div>
+                </div>
+              )}
+              <div className={"w-full flex flex-col gap-2.5"}>
+                <Label>Default Role</Label>
+                <Select
+                  ref={defaultRoleRef}
+                  className={"w-full"}
+                  placeholder={"Select Default Role"}
+                  options={assignedRoles}
+                  onChange={(e) => setNewDefaultRole(e)}
+                  filterOption={(option) =>
+                    option.value.id !== defaultRole.value.id
+                  }
+                />
+              </div>
             </div>
             <DrawerFooter>
               <Button>Submit</Button>
