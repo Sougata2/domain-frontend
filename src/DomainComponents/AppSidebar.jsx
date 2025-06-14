65863b51dbe6a557baf6a15c8edbcdcbdd1204ad
sougata <sougata19582001@gmail.com>
2025-06-14 15:11:06 +0530

    side bar header styling

--- src/DomainComponents/AppSidebar.jsx
+++ src/DomainComponents/AppSidebar.jsx
@@ -4,17 +4,38 @@ import {
   SidebarFooter,
   SidebarGroup,
   SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
 } from "@/components/ui/sidebar";
 import RecursiveSidebarMenu from "./RecursiveSidebarMenu";
 import useMenu from "@/hooks/use-menu";
 import { ModeToggle } from "@/components/mode-toggle";
 import { NavUser } from "@/components/nav-user";
+import { Link } from "react-router";
+import { MdDomain } from "react-icons/md";
 
 export function AppSidebar() {
   const { data: menus } = useMenu(1);
 
   return (
     <Sidebar>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:!p-1.5"
+            >
+              <Link to={"/home"}>
+                <MdDomain />
+                <span className="text-base font-semibold">Domain</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupLabel>Application</SidebarGroupLabel>
