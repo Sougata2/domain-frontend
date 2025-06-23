6c67b6fb51659831fb5e60574d0f0aaca8e5cde0
sougata <sougatadas@cdac.in>
2025-06-23 12:57:04 +0530

    add form input component.

---
+++ src/DomainComponents/FormInput.jsx
@@ -0,0 +1,24 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export default function FormInput({
+  label,
+  name,
+  register,
+  error,
+  validation,
+}) {
+  return (
+    <div className={"flex flex-col gap-2"}>
+      <Label htmlFor={name}>{label}</Label>
+      <div>
+        <Input {...register(name, { ...validation })} />
+        <div>
+          {error && (
+            <span className={"text-xs text-red-500"}>{error.message}</span>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
