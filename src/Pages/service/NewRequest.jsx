6bef2121c4b18c5d965fb0b0dedcfb484af0368a
sougata <sougata19582001@gmail.com>
2025-07-07 01:23:57 +0530

    added new page newRequest

---
+++ src/Pages/service/NewRequest.jsx
@@ -0,0 +1,93 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import FormSelect from "@/DomainComponents/Form/FormSelect";
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+
+export default function NewRequest() {
+  const {
+    reset,
+    watch,
+    formState: { errors },
+    handleSubmit,
+    control,
+  } = useForm();
+
+  const service = watch("service");
+
+  const [services, setServices] = useState([]);
+  const [subServices, setSubServices] = useState([]);
+
+  const fetchServices = useCallback(async () => {
+    try {
+      const response = await axios.get("/service/all");
+      const data = response.data;
+      setServices(data);
+    } catch (error) {
+      toast.error("Error", { description: error.message });
+    }
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      await fetchServices();
+    })();
+  }, [fetchServices]);
+
+  useEffect(() => {
+    if (service) {
+      const serviceObject = services.find((s) => s.id === service);
+      setSubServices(serviceObject?.subServices);
+    }
+  }, [service, services]);
+
+  async function submitHandler(data) {
+    console.log(data);
+  }
+
+  return (
+    <div className={"flex justify-center items-center"}>
+      <Card className={"w-3xl"}>
+        <CardHeader>
+          <CardTitle>Service Request</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form
+            onSubmit={handleSubmit(submitHandler)}
+            className={"flex justify-between flex-col gap-3 w-full"}
+          >
+            <div className="grid grid-cols-2 gap-5">
+              <FormSelect
+                label={"Service"}
+                name={"service"}
+                control={control}
+                error={errors.service}
+                options={services}
+                validation={{
+                  required: { value: true, message: "Service is required" },
+                }}
+                className="w-full"
+              />
+              <FormSelect
+                label={"SubService"}
+                name={"subService"}
+                control={control}
+                error={errors.subService}
+                options={subServices}
+                validation={{
+                  required: { value: true, message: "Sub Service is required" },
+                }}
+                className="w-full"
+              />
+            </div>
+            <div className="flex justify-end">
+              <Button>Save</Button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
