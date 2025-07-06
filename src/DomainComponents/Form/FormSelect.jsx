559b59b6248ec8407e270fad23f7948d6d86e249
sougata <sougata19582001@gmail.com>
2025-07-07 01:23:31 +0530

    new form Select compatible with useForm

---
+++ src/DomainComponents/Form/FormSelect.jsx
@@ -0,0 +1,57 @@
+import { Label } from "@/components/ui/label";
+import { Controller } from "react-hook-form";
+import {
+  Select,
+  SelectItem,
+  SelectValue,
+  SelectContent,
+  SelectTrigger,
+} from "@/components/ui/select";
+
+export default function FormSelect({
+  label,
+  name,
+  control,
+  error,
+  validation,
+  options,
+  className = "w-full",
+}) {
+  return (
+    <div className="flex flex-col gap-2">
+      <Label htmlFor={name}>{label}</Label>
+      <Controller
+        name={name}
+        control={control}
+        rules={validation}
+        render={({ field }) => {
+          const selectedOption = options?.find(
+            (o) => o.id === Number(field.value)
+          );
+
+          return (
+            <Select
+              onValueChange={(val) => field.onChange(Number(val))}
+              value={field.value?.toString() ?? ""}
+              disabled={!options?.length}
+            >
+              <SelectTrigger className={className}>
+                <SelectValue placeholder={`Select ${label}`}>
+                  {selectedOption?.name}
+                </SelectValue>
+              </SelectTrigger>
+              <SelectContent>
+                {options?.map((option) => (
+                  <SelectItem key={option.id} value={option.id.toString()}>
+                    {option.name}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          );
+        }}
+      />
+      {error && <div className={"text-red-400 text-sm"}>{error.message}</div>}
+    </div>
+  );
+}
