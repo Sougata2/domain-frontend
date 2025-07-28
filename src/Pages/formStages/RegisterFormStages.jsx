d3ed4bf8a163bcf134a2bb4277b898e2f2fcf12e
sougata <sougata19582001@gmail.com>
2025-07-29 01:10:39 +0530

    register form:  submit stage, render stage

--- src/Pages/formStages/RegisterFormStages.jsx
+++ src/Pages/formStages/RegisterFormStages.jsx
@@ -1,18 +1,18 @@
 import { Button } from "@/components/ui/button";
 import FormInput from "@/DomainComponents/FormInput";
 import FormSelect from "@/DomainComponents/FormSelect";
-import useMenu from "@/hooks/use-menu";
 import axios from "axios";
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Link } from "react-router";
 
 function RegisterFormStages() {
   const defaultValues = {
-    id: "",
     menu: "",
     stageOrder: "",
-    forms: "",
+    form: "",
   };
 
   const {
@@ -20,25 +20,95 @@ function RegisterFormStages() {
     register,
     reset,
     handleSubmit,
+    watch,
     formState: { errors },
   } = useForm({ defaultValues });
+  const form = watch("form");
 
-  const fetchMenus = useCallback(async () => {
+  const [subMenus, setSubMenus] = useState([]);
+  const [forms, setForms] = useState([]);
+  const [stages, setStages] = useState([]);
+  const [defaultTab, setDefaultTab] = useState("");
+
+  const fetchSubMenus = useCallback(async () => {
     try {
-      const response = await axios.get("/menu");
+      const response = await axios.get("/menu/sub-menus");
+      const data = response.data;
+      setSubMenus(data.map((d) => ({ label: d.name, value: d })));
     } catch (error) {
       toast.error("Error", { description: error.message });
     }
   }, []);
 
-  function handleOnSubmit(data) {
-    console.log(data);
+  const fetchForms = useCallback(async () => {
+    try {
+      const response = await axios.get("/form/all");
+      const data = response.data;
+      setForms(data.map((d) => ({ label: d.name, value: d })));
+    } catch (error) {
+      toast.error("Error", { description: error.message });
+    }
+  }, []);
+
+  const fetchStages = useCallback(async () => {
+    try {
+      const response = await axios.get(
+        `/form-stage/by-form-id/${form?.value.id}`
+      );
+      const data = response.data;
+      setStages(data);
+    } catch (error) {
+      toast.error("Error", { description: error.message });
+    }
+  }, [form]);
+
+  useEffect(() => {
+    (async () => {
+      await fetchSubMenus();
+    })();
+  }, [fetchSubMenus]);
+
+  useEffect(() => {
+    (async () => {
+      await fetchForms();
+    })();
+  }, [fetchForms]);
+
+  useEffect(() => {
+    if (form) {
+      (async () => {
+        await fetchStages();
+      })();
+    }
+  }, [fetchStages, form]);
+
+  useEffect(() => {
+    if (stages.length > 0) {
+      setDefaultTab(String(stages[0]?.id));
+    }
+  }, [stages, stages.length]);
+
+  async function handleOnSubmit(data) {
+    try {
+      const payload = {
+        ...data,
+        stageOrder: Number(data.stageOrder),
+        menu: { id: data.menu.value.id },
+        form: { id: data.form.value.id },
+      };
+
+      const _ = await axios.post("/form-stage", payload);
+      reset();
+      toast.success("Success", { description: "Stage Registered" });
+    } catch (error) {
+      toast.error("Error", { description: error.message });
+    }
   }
 
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center flex-col gap-30">
       <form onSubmit={handleSubmit(handleOnSubmit)}>
-        <div className="flex flex-col gap-2 w-md">
+        <div className="flex flex-col gap-3 w-md">
           <div>
             <FormSelect
               control={control}
@@ -46,14 +116,27 @@ function RegisterFormStages() {
               defaultValue={null}
               label={"Menu"}
               name={"menu"}
-              options={[
-                { label: "a", value: "A" },
-                { label: "b", value: "B" },
-              ]}
+              options={subMenus}
               validation={{
                 required: {
                   value: true,
                   message: "Menu is Required",
+                },
+              }}
+            />
+          </div>
+          <div>
+            <FormSelect
+              control={control}
+              error={errors.form}
+              defaultValue={null}
+              label={"Form"}
+              name={"form"}
+              options={forms}
+              validation={{
+                required: {
+                  value: true,
+                  message: "Form is Required",
                 },
               }}
             />
@@ -69,6 +152,10 @@ function RegisterFormStages() {
                   value: true,
                   message: "Stage Order is required",
                 },
+                min: {
+                  value: 1,
+                  message: "Stage order starts with 1",
+                },
               }}
               type={"number"}
             />
@@ -78,6 +165,32 @@ function RegisterFormStages() {
           </div>
         </div>
       </form>
+      {form && (
+        <div className="flex w-md justify-start">
+          <div className="flex flex-col gap-3">
+            <div className="text-4xl">Stage</div>
+            <Tabs
+              className="w-full"
+              value={defaultTab}
+              onValueChange={setDefaultTab}
+            >
+              <TabsList>
+                {stages.map((s) => (
+                  <TabsTrigger key={s.id} value={String(s.id)}>
+                    {s.menu.name}
+                  </TabsTrigger>
+                ))}
+              </TabsList>
+
+              {stages.map((s) => (
+                <TabsContent key={s.id} value={String(s.id)}>
+                  <Link to={`/${s.menu.url}`}>{s.menu.name}</Link>
+                </TabsContent>
+              ))}
+            </Tabs>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
