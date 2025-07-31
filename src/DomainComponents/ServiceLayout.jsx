14afa3e5a42c0f8e54565dc08654894baae14791
sougata <sougata19582001@gmail.com>
2025-08-01 01:18:15 +0530

    added stages Tabs

--- src/DomainComponents/ServiceLayout.jsx
+++ src/DomainComponents/ServiceLayout.jsx
@@ -1,32 +1,48 @@
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
-import { Outlet } from "react-router";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Outlet, useParams, NavLink } from "react-router";
+import { IoIosArrowForward } from "react-icons/io";
+import { fetchStages } from "@/state/stageSlice";
 
 function ServiceLayout() {
+  const dispatch = useDispatch();
+  const { referenceNumber } = useParams();
+  const { stages } = useSelector((state) => state.formStage);
+
+  useEffect(() => {
+    if (stages.length === 0) dispatch(fetchStages(referenceNumber));
+  }, [dispatch, referenceNumber, stages]);
+
   return (
     <div className="flex flex-col gap-2.5">
       <div className="flex justify-center items-center">
-        <Breadcrumb>
-          <BreadcrumbList>
-            <BreadcrumbItem>
-              <BreadcrumbLink href="/">Home</BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-              <BreadcrumbLink href="/components">Components</BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-              <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
+        {stages && (
+          <div className="flex items-center gap-2 flex-wrap">
+            {stages.map((stage, index) => (
+              <div key={stage.id} className="flex items-center gap-2">
+                <NavLink
+                  to={`/${stage.menu.url}/${referenceNumber}`}
+                  className={({ isActive }) =>
+                    `text-[15px] capitalize ${
+                      isActive
+                        ? "text-primary font-medium"
+                        : "text-muted-foreground"
+                    }`
+                  }
+                >
+                  {stage.menu.name}
+                </NavLink>
+
+                {index < stages.length - 1 && (
+                  <IoIosArrowForward
+                    className="text-muted-foreground pt-[1px]"
+                    size={"15px"}
+                  />
+                )}
+              </div>
+            ))}
+          </div>
+        )}
       </div>
       <div>
         <Outlet />
