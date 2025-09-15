9dea5394ae8ac530f57ed91e850e2e937760afbc
sougata <sougatadas@cdac.in>
2025-09-15 17:55:54 +0530

    created createJobCard page and added the component in the taskCardProvider

--- src/Pages/Task/TaskCards/TaskCardProvider.jsx
+++ src/Pages/Task/TaskCards/TaskCardProvider.jsx
@@ -1,15 +1,21 @@
+import CreateJobCard from "../job/CreateJobCard";
 import ActionCard from "./ActionCard";
 import ActionCardWithUpload from "./ActionCardWithUpload";
 import PaymentCard from "./PaymentCard";
 
-function TaskCardProvider({ referenceNumber, actionType }) {
+function TaskCardProvider({ referenceNumber, applicationData, actionType }) {
   switch (actionType) {
     case "ACTION":
       return <ActionCard referenceNumber={referenceNumber} />;
     case "ACTION_WITH_UPLOAD":
       return <ActionCardWithUpload referenceNumber={referenceNumber} />;
     case "CREATE_JOB_CARD":
-      return <div>Action form not decided yet</div>;
+      return (
+        <CreateJobCard
+          referenceNumber={referenceNumber}
+          applicationData={applicationData}
+        />
+      );
     case "PAYMENT":
       return <PaymentCard referenceNumber={referenceNumber} />;
     case "NONE":
---
+++ src/Pages/Task/job/CreateJobCard.jsx
@@ -0,0 +1,152 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { useCallback, useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+import { Badge } from "@/components/ui/badge";
+
+import PreviewDataBody from "@/DomainComponents/PreviewDataBody";
+import PreviewDataCell from "@/DomainComponents/PreviewDataCell";
+import axios from "axios";
+
+export function Confirm({ handleConfirm }) {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant={"link"}>Generate</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will create job card for this
+            device.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={handleConfirm}>Create</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
+
+function CreateJobCard({ referenceNumber, applicationData }) {
+  const [devices, setDevices] = useState([]);
+
+  const fetchDevices = useCallback(async () => {
+    try {
+      const response = await axios.get(
+        `/device/by-application-reference-number/${referenceNumber}`
+      );
+      const data = response.data;
+      setDevices(data);
+    } catch (error) {
+      toast.error("Error", { description: error.message });
+    }
+  }, [referenceNumber]);
+
+  useEffect(() => {
+    (async () => {
+      await fetchDevices();
+    })();
+  }, [fetchDevices]);
+
+  async function handleCreateJobCard(device) {
+    try {
+      const payload = {
+        device: { id: device.id },
+        status: { name: "JCG" },
+        assignee: { id: applicationData.assignee.id },
+        lab: { id: applicationData.lab.id },
+      };
+
+      const _ = await axios.post("/job", payload);
+      toast.success("Success", { description: "Job Created Successfully" });
+      await fetchDevices();
+    } catch (error) {
+      toast.error("Error", { description: error.message });
+    }
+  }
+
+  return (
+    <div className="flex flex-col gap-2 justify-center items-center">
+      {devices.map((device) => (
+        <Card key={device.id} className={"w-full"}>
+          <CardHeader>
+            <CardTitle>Job Card</CardTitle>
+            <CardDescription>create job card for this device</CardDescription>
+            <CardAction>
+              {device.job === null && (
+                <Confirm handleConfirm={() => handleCreateJobCard(device)} />
+              )}
+              {device.job !== null && (
+                <Badge variant={"secondary"}>Job Generated</Badge>
+              )}
+            </CardAction>
+          </CardHeader>
+          <CardContent>
+            <PreviewDataBody>
+              <PreviewDataCell label={"Device Name"} value={device.name} />
+              <PreviewDataCell
+                label={"Device Height"}
+                value={`${device.height} ${device.heightUnit}`}
+              />
+              <PreviewDataCell
+                label={"Device Weight"}
+                value={`${device.weight} ${device.weightUnit}`}
+              />
+              <PreviewDataCell
+                label={"Device Length"}
+                value={`${device.length} ${device.lengthUnit}`}
+              />
+              <PreviewDataCell label={"Quantity"} value={device.quantity} />
+              <PreviewDataCell
+                label={"Activities"}
+                value={
+                  <div className="flex gap-2">
+                    {device.activities.map((a) => (
+                      <Badge>{a.name}</Badge>
+                    ))}
+                  </div>
+                }
+              />
+              <PreviewDataCell
+                label={"Specifications"}
+                value={
+                  <div className="flex gap-2">
+                    {device.specifications.map((s) => (
+                      <Badge>{s.name}</Badge>
+                    ))}
+                  </div>
+                }
+              />
+            </PreviewDataBody>
+          </CardContent>
+          <CardFooter></CardFooter>
+        </Card>
+      ))}
+    </div>
+  );
+}
+
+export default CreateJobCard;
