adee30af2b5c55b263b831ffc6593d6d0260d855
sougata <sougata19582001@gmail.com>
2025-10-12 23:08:51 +0530

    added padding bottom

--- src/DomainComponents/Layout.jsx
+++ src/DomainComponents/Layout.jsx
@@ -9,7 +9,7 @@ export default function Layout() {
       <AlertBox />
       <SidebarProvider>
         <AppSidebar />
-        <main className="w-full">
+        <main className="w-full pb-4">
           <SidebarTrigger />
           <Outlet />
         </main>
