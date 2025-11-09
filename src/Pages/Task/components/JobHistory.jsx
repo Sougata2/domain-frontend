80b1201835be0f5b481c0437a2d05410b4faf611
sougata <sougata19582001@gmail.com>
2025-11-10 01:00:29 +0530

    added job history component

---
+++ src/Pages/Task/components/JobHistory.jsx
@@ -0,0 +1,131 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { useCallback, useEffect, useState } from "react";
+import { VscCircleSlash } from "react-icons/vsc";
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+import { toast } from "sonner";
+
+import Download from "@/DomainComponents/Download";
+import axios from "axios";
+
+function ViewComments({ comments, author }) {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="link">view</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Comments</DialogTitle>
+          <DialogDescription className={"py-3 font-mono"}>
+            {comments}
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className={"text-sm font-extralight"}>
+          by {author}
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+function JobHistory({ jobId }) {
+  const [workflowHistory, setWorkFlowHistory] = useState([]);
+
+  const fetchWorkFlowHistory = useCallback(async () => {
+    try {
+      const response = await axios.get(
+        `/job-workflow-history/by-job-id/${jobId}`
+      );
+      const data = response.data;
+      setWorkFlowHistory(data);
+    } catch (error) {
+      toast.error("Error", { description: error.message });
+    }
+  }, [jobId]);
+
+  useEffect(() => {
+    (async () => {
+      await fetchWorkFlowHistory();
+    })();
+  }, [fetchWorkFlowHistory]);
+
+  if (!workflowHistory.length) return;
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Job History</CardTitle>
+        <CardDescription>
+          list of all actions taken upon the Job
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <table className="table-bordered">
+          <thead>
+            <tr>
+              <th>S.No</th>
+              <th>Assigner</th>
+              <th>Action</th>
+              <th>Assignee</th>
+              <th>Comments</th>
+              <th>document</th>
+            </tr>
+          </thead>
+          <tbody>
+            {workflowHistory.map((h, i) => (
+              <tr key={h.id}>
+                <td>{i + 1}</td>
+                <td className="capitalize">{`${h.assigner.firstName} ${h.assigner.lastName}`}</td>
+                <td>
+                  <Badge variant={"secondary"}>
+                    {h.status.postDescription}
+                  </Badge>
+                </td>
+                <td className="capitalize">{`${h.assignee.firstName} ${h.assignee.lastName}`}</td>
+                <td>
+                  <div className="flex justify-center">
+                    {h.comments ? (
+                      <ViewComments
+                        comments={h.comments}
+                        author={`${h.assigner.firstName} ${h.assigner.lastName}`}
+                      />
+                    ) : (
+                      <VscCircleSlash className="text-slate-500" />
+                    )}
+                  </div>
+                </td>
+                <td>
+                  <div className="flex justify-center">
+                    {h.file ? (
+                      <Download fileId={h.file.id} />
+                    ) : (
+                      <VscCircleSlash className="text-slate-500" />
+                    )}
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default JobHistory;
